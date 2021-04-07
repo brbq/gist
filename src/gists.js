5c9a75747fc299bb54e37069087e547e40315cdf
jmabranquinho <jmabranquinho@gmail.com>
2021-04-07 09:50:57 +0100

    first

---
+++ src/gists.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const gists = [
+    {
+        id: 1,
+        title: 'Simple for Loop',
+        code: `
+        for (let i = 0; i < 3; i++) {
+            // TODO: logic
+        }
+        `,
+        keys: ['simple', 'for', 'loop', 'ts', 'typescript', 'react']
+    }
+];
+
+export default gists
